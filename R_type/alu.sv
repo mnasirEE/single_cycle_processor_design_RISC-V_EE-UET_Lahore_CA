184191abdb775d03410106821613850b4b4a9b5f
Muhammad Nasir <contactmnasirsaeed@gmail.com>
2024-03-02 20:27:42 +0500

    folder created

---
+++ R_type/alu.sv
@@ -0,0 +1,16 @@
+module alu (
+    input logic [31:0] operand_a, operand_b,
+    input logic [1:0] select_op,
+    output logic [31:0] result_out
+);
+
+always @(*)
+    case (select_op)
+       2'b00 : result_out = operand_a + operand_b;
+       2'b01 : result_out = operand_a - operand_b;
+       2'b10 : result_out = $signed(operand_a) + $signed(operand_b);
+       2'b11 : result_out = $signed(operand_a) + $signed(operand_b);
+       default: result_out = operand_a + operand_b;
+    endcase
+    
+endmodule
