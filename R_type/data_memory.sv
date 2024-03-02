655d96695850a3204c8d7bba6161f95fea102def
Muhammad Nasir <contactmnasirsaeed@gmail.com>
2024-03-03 02:41:12 +0500

    file updated

--- R_type/data_memory.sv
+++ R_type/data_memory.sv
@@ -2,18 +2,18 @@ module data_memory (
     input wire clk,
     input wire reset
 );
-
+     // byte alligned, word addressable
     // reg [31:0] ex2_memory [0:31]; // Memory for reading from file
     reg [31:0] ram_memory [31:0]; // Memory for writing to file
     
     // Reading from file
     initial begin
-        $readmemh("read.txt", ram_memory);
+        $readmemh("read_from.txt", ram_memory);
     end
     
     // Writing to file on positive clock edge
     always_ff @ (posedge clk) begin
-        $writememh("write.txt", ram_memory);
+        $writememh("write_in.txt", ram_memory);
     end
 
 endmodule
