99758a49da29af9dda363620e5efa0d3ed334c59
Muhammad Nasir <contactmnasirsaeed@gmail.com>
2024-03-17 00:54:39 +0500

    updated

--- R_I_type/data_memory.sv
+++ R_I_type/data_memory.sv
@@ -9,39 +9,64 @@ module data_memory #(parameter addr_data_width = 32, memory_width = 8, memory_he
     input logic [addr_data_width-1:0] addr,
     output logic [addr_data_width-1:0]data_out
 );
-     // byte alligned, word addressable
-    // reg [31:0] ex2_memory [0:31]; // Memory for reading from file
-    reg [memory_width - 1:0] ram_memory [memory_height - 1:0]; // Memory for writing to file
+
+// creating 2kB data memory
+reg [memory_width - 1:0] data_mem [0:memory_height - 1];
+
+always @(*) begin
+    $readmemh("read_from.txt", data_mem);
     
-    // Reading from file
-    initial begin
-        
-        $readmemh("read_from.txt", ram_memory);
-        if (r_en)
-            // Concatenate bytes to form a word
-            data_out = {ram_memory[addr+: 4]};
+end
+
+always @(*) begin
+    if (r_en) begin
+        data_out[7:0] = data_memory[addr];
+        data_out[15:8] = data_memory[addr + 1]; // suppose up stack
+        data_out[23:16] = data_memory[addr + 1];
+        data_out[31:24] = data_memory[addr + 1];
     end
-    
-    // Writing to file on positive clock edge
-    always_ff @ (posedge clk, posedge wr_en) begin
-        if (wr_en)
-            $writememh("write_in.txt", ram_memory);
-            //Write data to memory
-            memory[byte_address] <= data_in[7:0];
-            memory[byte_address + 1] <= data_in[15:8];
-            memory[byte_address + 2] <= data_in[23:16];
-            memory[byte_address + 3] <= data_in[31:24];
-    end
+end
+
+
+
 
 endmodule
 
-// if (write_enable) begin
-//                 // Write data to memory
-//                 memory[byte_address] <= data_in[7:0];
-//                 memory[byte_address + 1] <= data_in[15:8];
-//                 memory[byte_address + 2] <= data_in[23:16];
-//                 memory[byte_address + 3] <= data_in[31:24];
-//             end
 
-// Concatenate bytes to form a word
-// assign instruction = {memory[byte_address * 4 +: 4]};
+
+//      // byte alligned, word addressable
+//     // reg [31:0] ex2_memory [0:31]; // Memory for reading from file
+//     reg [memory_width - 1:0] ram_memory [memory_height - 1:0]; // Memory for writing to file
+    
+//     // Reading from file
+//     initial begin
+        
+//         $readmemh("read_from.txt", ram_memory);
+//         if (r_en)
+//             // Concatenate bytes to form a word
+//             data_out = {ram_memory[addr+: 4]};
+//     end
+    
+//     // Writing to file on positive clock edge
+//     always_ff @ (posedge clk, posedge wr_en) begin
+//         if (wr_en)
+//             $writememh("write_in.txt", ram_memory);
+//             //Write data to memory
+//             memory[byte_address] <= data_in[7:0];
+//             memory[byte_address + 1] <= data_in[15:8];
+//             memory[byte_address + 2] <= data_in[23:16];
+//             memory[byte_address + 3] <= data_in[31:24];
+//     end
+
+// endmodule
+
+// // if (write_enable) begin
+// //                 // Write data to memory
+// //                 memory[byte_address] <= data_in[7:0];
+// //                 memory[byte_address + 1] <= data_in[15:8];
+// //                 memory[byte_address + 2] <= data_in[23:16];
+// //                 memory[byte_address + 3] <= data_in[31:24];
+// //             end
+
+// // Concatenate bytes to form a word
+// // assign instruction = {memory[byte_address * 4 +: 4]};
